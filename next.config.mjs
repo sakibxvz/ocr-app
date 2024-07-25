845cab1b61193737d2cbed6c372c75dccd33a832
sakibxvz <sakibxvz@gmail.com>
2024-07-25 14:44:14 +0600

    Intial Basic App done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback = {
+				fs: false,
+				path: false,
+				crypto: false,
+			};
+			config.plugins.push(new NodePolyfillPlugin());
+		}
+
+		return config;
+	},
+};
 
 export default nextConfig;
